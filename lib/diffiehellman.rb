1977b0583be3cea65d17c86031de600288ee3758
alu0100948097 <alu0100948097@ull.edu.es>
2018-04-12 12:51:09 +0100

    Solicitud de datos al usuario

--- lib/diffiehellman.rb
+++ lib/diffiehellman.rb
@@ -12,3 +12,38 @@ class Diffiehellman
 	end
     
 end
+
+@test=Diffiehellman.new
+    puts
+    puts "PRÁCTICA: INTERCAMBIO DE CLAVES DE DIFFIE-HELLMAN"
+    puts
+    print "Introduzca el número primo p: "
+    p=gets.chomp.to_i
+    while p<=0
+        system "clear"
+        if p==0
+            puts "El número primo no puede ser igual a 0".red
+        else
+            puts "El número primo no puede ser negativo".red
+        end
+        puts
+        print "Introduzca el número primo p: "
+        p=gets.chomp.to_i
+    end
+    @test.p=p
+    print "Introduzca alpha ⍺: "
+    ⍺=gets.chomp.to_i
+    while ⍺>p
+        system "clear"
+        puts "El alpha ⍺ introducido es mayor a p".red
+        puts
+        print "Introduzca alpha ⍺: "
+        ⍺=gets.chomp.to_i
+    end
+    @test.⍺=⍺
+    print "Introduzca el secreto xa: "
+    xa=gets.chomp.to_i
+    @test.xa=xa
+    print "Introduzca el secreto xb: "
+    xb=gets.chomp.to_i
+    @test.xb=xa
