60b8bb420a5cf20c3339374f7f122e6dee4ca6f8
alu0100948097 <alu0100948097@ull.edu.es>
2018-04-12 12:50:39 +0100

    Inicialización de clase

---
+++ lib/diffiehellman.rb
@@ -0,0 +1,14 @@
+require 'colorize'
+
+class Diffiehellman
+    
+    attr_writer :p, :⍺, :xa, :xb
+    
+    def initialize
+        @p=0
+        @⍺=0
+        @xa=0
+        @xb=0
+	end
+    
+end
